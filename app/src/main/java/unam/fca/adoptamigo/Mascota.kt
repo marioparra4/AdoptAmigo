8206475a2e7603db9fce49d0e58855d35f5027ee
Paola Monserrat Brilt Jiménez <paolabj97@gmail.com>
2021-05-31 14:53:17 -0500

    Clase mascota con los datos de la mascota.

---
+++ app/src/main/java/unam/fca/adoptamigo/Mascota.kt
@@ -0,0 +1,10 @@
+package unam.fca.adoptamigo
+
+class Mascota(var imagen: Int,
+              var raza: String,
+              var edad: String,
+              var genero: String,
+              var ubicacion: String,
+              var color: String,
+              var descripcion: String) {
+}
