1ea4d4ab20524a6fe5d3d7383e3d6c77e6d5f998
Mario Parra Huerta <79287092+marioparra4@users.noreply.github.com>
2021-06-01 17:11:56 -0500

    MainActivity.kt funciona el menu AdoptarActivity.kt se cambio el nombre de actividad 2 a AdoptarActivity.kt

--- app/src/main/java/unam/fca/adoptamigo/AdoptarActivity.kt
+++ app/src/main/java/unam/fca/adoptamigo/AdoptarActivity.kt
@@ -3,9 +3,9 @@ package unam.fca.adoptamigo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class AdoptarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_adoptar)
     }
 }
--- app/src/main/java/unam/fca/adoptamigo/MainActivity.kt
+++ app/src/main/java/unam/fca/adoptamigo/MainActivity.kt
@@ -1,11 +1,40 @@
 package unam.fca.adoptamigo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            menuInflater.inflate(R.menu.menu, menu)
+            return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when(id){
+            R.id.item1 -> {
+                val intent = Intent(this, AdoptarActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.item2 -> {
+                val intent = Intent(this, FormularioActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
