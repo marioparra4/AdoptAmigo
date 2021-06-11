19dd2a50c3f9eabcd10a19f4fb593985b644cce0
Mario Parra Huerta <79287092+marioparra4@users.noreply.github.com>
2021-06-10 19:27:42 -0500

    activity_formulario.xml Se reacomodo el formulario, se ingreso biton y se quito imagebutton, los ems los puse con 11 AndroidManifest.xml se agrego el uso de permisos para acceder a galeria. FormularioActivity.kt se mapea los elementos de la vista, se abre un intent donde seleccionas la imagen y la pone en el imageview del formulario

--- app/src/main/java/unam/fca/adoptamigo/FormularioActivity.kt
+++ app/src/main/java/unam/fca/adoptamigo/FormularioActivity.kt
@@ -1,11 +1,44 @@
 package unam.fca.adoptamigo
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import java.net.URI
 
 class FormularioActivity : AppCompatActivity() {
+    private lateinit var imagen : ImageView
+    private lateinit var boton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario)
+
+        imagen = findViewById(R.id.inserta)
+        boton = findViewById(R.id.botonFoto)
+
+        boton.setOnClickListener {
+            cargarImagen()
+        }
+    }
+
+    private fun cargarImagen() {
+        val intent  = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.setType("image/")
+        startActivityForResult(Intent.createChooser(intent,"Seleccione la aplicacion"),10)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK){
+            val uri = data?.data
+            imagen.setImageURI(uri)
+
+        }
+
     }
 }
